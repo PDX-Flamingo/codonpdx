68c459352d06f3b726e6cf52c22691f08f303eab
Robert <robert.konell@gmail.com>
2014-07-05 15:38:51 -0700

    Created a TaskScheduler class for scheduling tasks to the computer clusters.  Uses RabbitMQ as the message broker, and Celery is on the backend.

--- src/edu/pdx/codonpdx/TaskScheduler.java
+++ src/edu/pdx/codonpdx/TaskScheduler.java
@@ -1,6 +1,6 @@
 package edu.pdx.codonpdx;
 
-import com.rabbitmq.client.*;
+import com.rabbitmq.client.*; // get this from http://www.rabbitmq.com/java-client.html
 
 import java.io.IOException;
 import java.util.Formatter;
@@ -21,6 +21,7 @@ public class TaskScheduler {
     //Message strings
     private String testString = "{\"expires\": null, \"utc\": true, \"args\": [%1$d], \"chord\": null, \"callbacks\": null, \"errbacks\": null, \"taskset\": \"%2$s\", \"id\": \"%3$s\", \"retries\": 0, \"task\": \"%4$s\", \"timelimit\": [null, null], \"eta\": null, \"kwargs\": {}}";
 
+    // Constrctor
     public TaskScheduler (String queue, String host) throws IOException{
         QUEUE_NAME = queue;
         HOST = host;
@@ -28,6 +29,7 @@ public class TaskScheduler {
         openConnect();
     }
 
+    // Opens a connection based on set queue and host
     public void openConnect() throws IOException {
         factory = new ConnectionFactory();
         factory.setHost(HOST);
@@ -35,22 +37,26 @@ public class TaskScheduler {
         channel = connection.createChannel();
     }
 
+    // Schedule a task with a random UUID for the id.  id is returned
+    // so that the results of the message can be retrieved if needed
     public String scheduleTask(String task) throws IOException {
         StringBuilder sb = new StringBuilder();
-        Formatter formatter = new Formatter(sb, Locale.US);
-        String taskList = UUID.randomUUID().toString();
-        String id = UUID.randomUUID().toString();
+        Formatter formatter = new Formatter(sb, Locale.US);  // Some of this should be moved to separate methods
+        String taskList = UUID.randomUUID().toString();  // find out if taskList is even needed
+        String id = UUID.randomUUID().toString();  // Need to
         String message = formatter.format(testString, 5, taskList, id, task).toString();
         channel.basicPublish(QUEUE_NAME, QUEUE_NAME, properties, message.getBytes("ASCII"));
         System.out.println(" Sent : " + message);
         return id;
     }
 
+    // method provided to clean up when user finished
     public void closeConnect() throws IOException {
         channel.close();
         connection.close();
     }
 
+    // main method just for testing
     public static void main(String[] argv)
             throws java.io.IOException {
         TaskScheduler ts = new TaskScheduler("celery", "localhost");
